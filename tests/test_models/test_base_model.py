a72cb77d95be57a4b9a872c32c2b9ece66e40bd6
josfam <josephamukun@tutanota.com>
2024-03-09 12:32:39 +0300

    Docs: Add docs to all test functions

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,24 +13,29 @@ class TestBaseModel(unittest.TestCase):
     """Test cases for BaseModel behaviour"""
 
     def setUp(self):
+        """Routine to be performed before every test"""
         self.b = BM()
 
     def tearDown(self):
+        """Routine to be performed after every test"""
         storage.all().clear()
 
     def test_a_new_base_must_have_a_unique_id(self):
+        """A new base must have a unique id"""
         b = self.b
         b2 = BM()
         self.assertIsInstance(b.id, str)
         self.assertNotEqual(b.id, b2.id)
 
     def test_creation_time_must_be_earlier_than_update_time(self):
+        """Creation time must be earlier (or equal) to update time"""
         b = self.b
         self.assertIsInstance(b.created_at, datetime)
         self.assertIsInstance(b.updated_at, datetime)
         self.assertLessEqual(b.created_at, b.updated_at)
 
     def test_save_changes_only_update_time(self):
+        """`save` changes only update time"""
         b = self.b
         first_creation = b.created_at
         first_update = b.updated_at
@@ -42,6 +47,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertLessEqual(first_update, b.updated_at)
 
     def test_to_dict_adds_a_new__class__key(self):
+        """`to_dict` adds a new `__class__` key to the obect's dictionary"""
         b = self.b
         old_attrs = b.__dict__.keys()
         self.assertFalse('__class__' in old_attrs)
@@ -49,6 +55,9 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue('__class__' in new_attrs)
 
     def test_to_dict_makes_dates_into_iso_format(self):
+        """`to_dict` makes dates into an iso-formatted string, of the format:
+        %Y-%m-%dT%H:%M:%S.%f
+        """
         b = self.b
         created_iso = b.created_at.isoformat()
         updated_iso = b.updated_at.isoformat()
@@ -57,18 +66,25 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(b_dict['updated_at'], updated_iso)
 
     def test_a_base_from_a_dict_is_a_base_instance(self):
+        """A Base created from a dict, is in-fact a Base object"""
         b = self.b
         b_dict = b.to_dict()
         b1 = BM(b_dict)
         self.assertIsInstance(b1, BM)
 
     def test_a_base_from_a_dict_has_no__class__key(self):
+        """A Base created from a dictionary discards the `__class__` key
+        from its own object dictionary
+        """
         b = self.b
         b_dict = b.to_dict()
         b1 = BM(b_dict)
         self.assertTrue('__class__' not in b1.__dict__.keys())
 
     def test_a_base_from_a_dict_turns_string_times_to_datetime_times(self):
+        """A Base created from a dictionary turns the iso-formatted dates
+        back into proper datetime dates
+        """
         b = self.b
         self.assertIsInstance(b.created_at, datetime)
         self.assertIsInstance(b.updated_at, datetime)
--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -11,12 +11,15 @@ class TestStorage(unittest.TestCase):
     """Test cases for TestStorage behavior"""
 
     def tearDown(self):
+        """Routine to be performed after every test"""
         storage.all().clear()
 
     def test_storage_has_no_objects_if_no_objects_have_been_created(self):
+        """Storage must be empty if nothing has been stored yet"""
         self.assertTrue(len(storage.all()) == 0)
 
     def test_new_base_objects_are_stored_upon_creation(self):
+        """Newly created Base objects are instantly stored"""
         self.assertTrue(len(storage.all()) == 0)
         b = BM()
         self.assertTrue(len(storage.all()) == 1)
@@ -25,6 +28,7 @@ class TestStorage(unittest.TestCase):
         self.assertTrue(len(storage.all()) == 3)
 
     def test_objects_are_stored_with_the_correct_format(self):
+        """Base objects are correctly represented in the storage dictionary"""
         b = BM()
         id = b.id
         name = 'BaseModel'
