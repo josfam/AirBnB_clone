9f71f6a33574dae99f00913a47f8fa22f8690f4a
josfam <josephamukun@tutanota.com>
2024-03-10 16:14:42 +0300

    Docs: Add more detail to models `init` file

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
-"""Defines models to be used in the project"""
+"""Defines models to be used in the project, most of which do inherit their
+functionality from BaseModel.
+Objects are reconstructed from whatever storage engine is being used, at the
+very start of the program.
+Storage is globally available to other modules to use.
+"""
 
 from .engine.file_storage import FileStorage
 
