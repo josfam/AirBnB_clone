bac1460bba5e858ed8fbf27f2ff0557631bffd79
josfam <josephamukun@tutanota.com>
2024-03-10 16:03:05 +0300

    Docs: Add more detail to engine `init` file

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,1 +1,6 @@
-"""File storage engine for storing objects of the project"""
+"""File storage engine for storing objects that are used throughout this
+project.
+Ideally, storage is instantiated when the program is run, and is
+then used to store objects into a storage space of some kind (a JSON file,
+or database) and retrieves them from that storage space.
+"""
